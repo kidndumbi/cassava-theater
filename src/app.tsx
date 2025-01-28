b58ea97bc1f2275c1d44c692d9f48fac6fdba70d
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2025-01-28 15:42:50 -0500

    add MUI

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material";
+import theme from "./renderer/theme";
+import Button from "@mui/material/Button";
 
 const App = () => {
   const [message, setMessage] = useState("Hello from React!");
@@ -8,7 +12,7 @@ const App = () => {
     // This code runs after the component mounts
     console.log("Component mounted");
     // You can update the state or perform other side effects here
-    setMessage("Hello from useEffect!"); 
+    setMessage("Hello from useEffect!");
 
     // Cleanup function (optional)
     return () => {
@@ -16,8 +20,20 @@ const App = () => {
     };
   }, []); // Empty dependency array means this runs once after initial render
 
-  return <h2>{message}</h2>;
+  return (
+    <>
+      <h2>{message}</h2>
+      <Button color="primary">Cancel</Button>
+    </>
+  );
 };
 
 const root = createRoot(document.body);
-root.render(<App />);
+root.render(
+  <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </>
+);
