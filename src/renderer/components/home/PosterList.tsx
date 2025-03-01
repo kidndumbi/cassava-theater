48640145ac61709e3c0e04239cd5a4d16e8b53d2
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2025-02-28 19:12:17 -0500

    resume scrolling

--- src/renderer/components/home/PosterList.tsx
+++ src/renderer/components/home/PosterList.tsx
@@ -1,14 +1,104 @@
-import { Box } from "@mui/material";
-import { ReactNode } from "react";
+import { Box, IconButton } from "@mui/material";
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import theme from "../../theme";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 interface ListProps {
   children: ReactNode;
 }
 
 export const PosterList = ({ children }: ListProps) => {
+  const [scrollPosition, setScrollPosition] = useState("beginning");
+  const boxRef = useRef<HTMLDivElement>(null); 
+
+
+
+  useEffect(() => {
+    if (
+      boxRef.current &&
+      boxRef.current.scrollWidth <= boxRef.current.clientWidth
+    ) {
+      setScrollPosition("no-scroll");
+      console.log("scrollPosition: no-scroll");  
+    } else {
+      console.log("scrollPosition: ", scrollPosition);
+    }
+  }, [scrollPosition]);
+
+  const handleScroll = useCallback((event: React.UIEvent<HTMLDivElement>) => {
+    const target = event.currentTarget;
+    if (target.scrollWidth <= target.clientWidth) {
+      setScrollPosition("no-scroll");
+      return;
+    }
+    let position = "middle";
+    if (target.scrollLeft === 0) {
+      position = "beginning";
+    } else if (
+      Math.abs(target.scrollWidth - target.clientWidth - target.scrollLeft) < 1
+    ) {
+      position = "end";
+    }
+    setScrollPosition(position);
+    console.log(position);
+  }, []);
+
+  const scrollLeft = () => {
+    if (boxRef.current) {
+      boxRef.current.scrollBy({ left: -200, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (boxRef.current) {
+      boxRef.current.scrollBy({ left: 200, behavior: "smooth" });
+    }
+  };
+
   return (
-    <Box className="flex gap-2 overflow-x-auto whitespace-nowrap">
-      {children}
+    <Box className="relative">
+      {scrollPosition !== "beginning" && scrollPosition !== "no-scroll" && (
+        <Box className="absolute inset-y-0 left-6 top-32 z-10">
+          <IconButton
+            onClick={scrollLeft}
+            sx={{
+              color: theme.customVariables.appWhiteSmoke,
+              backgroundColor: `${theme.customVariables.appDark}CC`, // 80% opacity
+              "&:hover": {
+                backgroundColor: `${theme.customVariables.appDark}CC`,
+              },
+            }}
+            className="h-12 w-12"
+          >
+            <ArrowBackIosNewIcon />
+          </IconButton>
+        </Box>
+      )}
+      <Box
+        ref={boxRef}
+        className="flex gap-2 overflow-x-auto whitespace-nowrap"
+        onScroll={handleScroll}
+      >
+        {children}
+      </Box>
+      {scrollPosition !== "end" && scrollPosition !== "no-scroll" && (
+        <Box className="absolute inset-y-0 right-6 top-32 z-10">
+          <IconButton
+            onClick={scrollRight}
+            sx={{
+              color: theme.customVariables.appWhiteSmoke,
+              backgroundColor: `${theme.customVariables.appDark}CC`, // 80% opacity
+              "&:hover": {
+                backgroundColor: `${theme.customVariables.appDark}CC`,
+              },
+            }}
+            className="h-12 w-12"
+          >
+            <ArrowForwardIosIcon />
+          </IconButton>
+        </Box>
+      )}
     </Box>
   );
 };
