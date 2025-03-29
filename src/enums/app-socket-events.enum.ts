4a95747de48c548e40fa0bb1797e1d280688149e
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2025-03-28 21:28:02 -0400

    refactor

--- src/enums/app-socket-events.enum.ts
+++ src/enums/app-socket-events.enum.ts
@@ -1,8 +1,9 @@
 export enum AppSocketEvents {
   REMOTE_COMMAND = "remote-command",
   SET_PLAYING = "set-playing",
-  GET_VIDEOS_DATA = 'get-videos-data',
-  GET_FOLDER_DETAILS = 'get-folder-details',
-  SET_CURRENTTIME = 'set-currenttime',
+  GET_VIDEOS_DATA = "get-videos-data",
+  GET_FOLDER_DETAILS = "get-folder-details",
+  GET_VIDEO_DETAILS = "get-video-details",
+  SET_CURRENTTIME = "set-currenttime",
   GET_SETTINGS = "get-settings",
 }
