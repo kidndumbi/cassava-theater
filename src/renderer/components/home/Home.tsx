3e51d8cfccd8b6b53cf7dd3ca644b417e475b96a
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2025-04-28 20:08:41 -0400

    refactor

--- src/renderer/components/home/Home.tsx
+++ src/renderer/components/home/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 import RefreshIcon from "@mui/icons-material/Refresh";
-import { VideoDataModel } from "../../../models/videoData.model";
 import { useTvShows } from "../../hooks/useTvShows";
 import useHandlePosterClick from "../../hooks/useHandlePosterClick";
 import { useVideoListLogic } from "../../hooks/useVideoListLogic";
@@ -9,36 +8,40 @@ import ResumeTvShowLists from "./ResumeTvShowLists";
 import ResumeMovieList from "./ResumeMovieList";
 import { WatchLaterList } from "./WatchLaterList";
 import AppIconButton from "../common/AppIconButton";
-import { useRecentlyWatchedVideosQuery } from "../../hooks/useVideoData.query";
+import {
+  useRecentlyWatchedVideosQuery,
+  useVideoDataQuery,
+} from "../../hooks/useVideoData.query";
+import { useSettings } from "../../hooks/useSettings";
 
 interface HomePageProps {
   style?: React.CSSProperties;
-  movies: VideoDataModel[];
-  tvShows: VideoDataModel[];
-  loadingTvShows: boolean;
-  loadingMovies: boolean;
   refreshData: () => void;
   menuId: string;
 }
 
 export const HomePage: React.FC<HomePageProps> = ({
   style,
-  movies,
-  tvShows,
-  loadingTvShows,
-  loadingMovies,
   refreshData,
   menuId,
 }) => {
   const theme = useTheme();
   const { setCurrentVideo } = useVideoListLogic();
   const { getSingleEpisodeDetails } = useTvShows();
+  const { settings } = useSettings();
 
-  const { data: sortedMovies } = useRecentlyWatchedVideosQuery({
-    videoType: "movies",
-  });
-  const { data: sortedTvShows } = useRecentlyWatchedVideosQuery({
-    videoType: "tvShows",
+  const { data: sortedMovies, isLoading: loadingMovies } =
+    useRecentlyWatchedVideosQuery({
+      videoType: "movies",
+    });
+  const { data: sortedTvShows, isLoading: loadingTvShows } =
+    useRecentlyWatchedVideosQuery({
+      videoType: "tvShows",
+    });
+
+  const { data: movies, isLoading: loadingWatchLater } = useVideoDataQuery({
+    filePath: settings?.movieFolderPath || "",
+    category: "movies",
   });
 
   const { handlePosterClick, loadingItems } = useHandlePosterClick(
@@ -68,7 +71,7 @@ export const HomePage: React.FC<HomePageProps> = ({
       <AppIconButton
         tooltip="Refresh"
         onClick={refreshData}
-        disabled={loadingMovies && loadingTvShows} // Disable the button when both are loading to prevent multiple calls
+        disabled={loadingMovies && loadingTvShows}
       >
         <RefreshIcon />
       </AppIconButton>
@@ -93,9 +96,8 @@ export const HomePage: React.FC<HomePageProps> = ({
       <Box>
         <WatchLaterList
           handlePosterClick={handlePosterClick}
-          loadingMovies={loadingMovies}
+          loadingMovies={loadingWatchLater}
           movies={movies}
-          tvShows={tvShows}
         />
       </Box>
     </Box>
