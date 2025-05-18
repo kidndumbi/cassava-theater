0c8415d14f7a45076cc14717edabaefe974b3038
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2025-05-17 22:23:05 -0400

    apply drag and drop to custom folders

--- src/renderer/components/main-menu/MainMenu.tsx
+++ src/renderer/components/main-menu/MainMenu.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { Box, Button, List, useTheme } from "@mui/material";
-
+import { Box, Button, List } from "@mui/material";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { MenuItem } from "../../../models/menu-item.model";
+import { SettingsModel } from "../../../models/settings.model";
+import { useGetAllSettings } from "../../hooks/settings/useGetAllSettings";
+import { useSetSetting } from "../../hooks/settings/useSetSetting";
+import { DraggableMenuItem } from "./DraggableMenuItem";
+import buttonStyle from "./buttonStyle";
 
 interface MainMenuProps {
   menuItems: MenuItem[];
@@ -17,19 +23,8 @@ const MainMenu: React.FC<MainMenuProps> = ({
   onActiveMenuItemChange,
   onSettingsClick,
 }) => {
-  const theme = useTheme();
-
-  const buttonStyle = {
-    backgroundColor: theme.customVariables.appDarker,
-    color: theme.customVariables.appWhiteSmoke,
-    boxShadow: "none",
-    border: "none",
-    marginBottom: "10px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-  };
+  const { data: { folders } = {} as SettingsModel } = useGetAllSettings();
+  const { mutateAsync: setSetting } = useSetSetting();
 
   const handleButtonClick = (item: MenuItem) => {
     onActiveMenuItemChange(item);
@@ -37,66 +32,47 @@ const MainMenu: React.FC<MainMenuProps> = ({
   };
 
   return (
-    <List sx={{ width: "130px", borderRight: "1px solid #ccc" }}>
-      {menuItems.map((item) => {
-        const isActive = item.label === activeMenuItem.label;
-        const activeStyle = isActive
-          ? { backgroundColor: theme.palette.primary.main }
-          : {};
-
-        // Only clone if icon is a valid ReactElement
-        const icon =
-          isActive &&
-          item.menuType === "default" &&
-          React.isValidElement(item.icon)
-            ? React.cloneElement(item.icon, {
-                // Use 'as any' to bypass TS error for 'sx'
-                sx: {
-                  ...(typeof (item.icon as any).props?.sx === "object"
-                    ? { ...(item.icon as any).props.sx }
-                    : {}),
-                  color: theme.customVariables.appWhiteSmoke,
-                },
-              } as any)
-            : item.icon;
-
-        return (
-          <Button
-            key={item.label}
-            style={{
-              ...buttonStyle,
-              ...activeStyle,
-              borderTopRightRadius: "0px",
-              borderBottomRightRadius: "0px",
+    <DndProvider backend={HTML5Backend}>
+      <List sx={{ width: "130px", borderRight: "1px solid #ccc" }}>
+        {menuItems.map((menu) => (
+          <DraggableMenuItem
+            key={menu.label}
+            menu={menu}
+            idx={menuItems.indexOf(menu)}
+            activeMenuItem={activeMenuItem}
+            handleButtonClick={handleButtonClick}
+            switchCustomFolderPosition={(id1: string, id2: string) => {
+              const folder1 = folders?.find((item) => item.id === id1);
+              const folder2 = folders?.find((item) => item.id === id2);
+              if (folder1 && folder2 && Array.isArray(folders)) {
+                const index1 = folders.indexOf(folder1);
+                const index2 = folders.indexOf(folder2);
+                const newFolders = [...folders];
+                newFolders[index1] = folder2;
+                newFolders[index2] = folder1;
+                setSetting({
+                  key: "folders",
+                  value: newFolders,
+                });
+              }
             }}
-            onClick={() => handleButtonClick(item)}
+          />
+        ))}
+
+        <Button style={{ ...buttonStyle }} onClick={onSettingsClick}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
           >
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              {icon}
-              <span>{item.label}</span>
-            </Box>
-          </Button>
-        );
-      })}
-      <Button style={{ ...buttonStyle }} onClick={onSettingsClick}>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-          }}
-        >
-          <SettingsIcon />
-          <span>Settings</span>
-        </Box>
-      </Button>
-    </List>
+            <SettingsIcon />
+            <span>Settings</span>
+          </Box>
+        </Button>
+      </List>
+    </DndProvider>
   );
 };
 
---
+++ src/renderer/components/main-menu/buttonStyle.ts
@@ -0,0 +1,15 @@
+import theme from "../../theme";
+
+const buttonStyle = {
+  backgroundColor: theme.customVariables.appDarker,
+  color: theme.customVariables.appWhiteSmoke,
+  boxShadow: "none",
+  border: "none",
+  marginBottom: "10px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+};
+
+export default buttonStyle;
