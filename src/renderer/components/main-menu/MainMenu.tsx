3e4ee94f408dad81948b12468b3952155a121239
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2025-05-23 16:35:16 -0400

    refactor

--- src/renderer/components/main-menu/MainMenu.tsx
+++ src/renderer/components/main-menu/MainMenu.tsx
@@ -2,13 +2,8 @@ import React from "react";
 import { Box, Button, List } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { MenuItem } from "../../../models/menu-item.model";
-import { SettingsModel } from "../../../models/settings.model";
-import { useGetAllSettings } from "../../hooks/settings/useGetAllSettings";
-import { useSetSetting } from "../../hooks/settings/useSetSetting";
 import { MainMenuItem } from "./MainMenuItem";
 import buttonStyle from "./buttonStyle";
-import { useConfirmation } from "../../contexts/ConfirmationContext";
-import { useSnackbar } from "../../contexts/SnackbarContext";
 
 interface MainMenuProps {
   menuItems: MenuItem[];
@@ -23,61 +18,20 @@ const MainMenu: React.FC<MainMenuProps> = ({
   onActiveMenuItemChange,
   onSettingsClick,
 }) => {
-  const { data: { folders } = {} as SettingsModel } = useGetAllSettings();
-  const { mutateAsync: setSetting } = useSetSetting();
-  const { openDialog } = useConfirmation();
-  const { showSnackbar } = useSnackbar();
-
   const handleButtonClick = (item: MenuItem) => {
     onActiveMenuItemChange(item);
     item.handler(item);
   };
 
-  const switchCustomFolderPosition = (id1: string, id2: string) => {
-    const folder1 = folders?.find((item) => item.id === id1);
-    const folder2 = folders?.find((item) => item.id === id2);
-    if (folder1 && folder2 && Array.isArray(folders)) {
-      const index1 = folders.indexOf(folder1);
-      const index2 = folders.indexOf(folder2);
-      const newFolders = [...folders];
-      newFolders[index1] = folder2;
-      newFolders[index2] = folder1;
-      setSetting({
-        key: "folders",
-        value: newFolders,
-      });
-    }
-  };
-
-  const handleDeleteFolder = async (folderId: string) => {
-    const dialogDecision = await openDialog(
-      undefined,
-      undefined,
-      "Are you sure you want to delete this folder?",
-    );
-    if (dialogDecision === "Ok") {
-      const updatedFolders = (folders || []).filter(
-        (folder) => folder.id !== folderId,
-      );
-      await setSetting({
-        key: "folders",
-        value: updatedFolders,
-      });
-      showSnackbar("Folder deleted successfully", "success");
-    }
-  };
-
   return (
     <>
       <List sx={{ width: "130px", borderRight: "1px solid #ccc" }}>
-        {menuItems.map((menu, idx) => (
+        {menuItems.map((menu) => (
           <MainMenuItem
             key={menu.label}
             menu={menu}
-            idx={idx}
             activeMenuItem={activeMenuItem}
             handleButtonClick={handleButtonClick}
-            switchCustomFolderPosition={switchCustomFolderPosition}
           />
         ))}
 
--- src/renderer/components/main-menu/MainMenuItem.tsx
+++ src/renderer/components/main-menu/MainMenuItem.tsx
@@ -1,28 +1,17 @@
 import React from "react";
 import { Box, Button } from "@mui/material";
-import { useDrag, useDrop } from "react-dnd";
 import { MenuItem } from "../../../models/menu-item.model";
 import theme from "../../theme";
 import buttonStyle from "./buttonStyle";
 
-interface DragItem {
-  index: number;
-  type: string;
-  menu: MenuItem;
-}
-
 export const MainMenuItem = ({
   menu,
-  idx,
   activeMenuItem,
   handleButtonClick,
-  switchCustomFolderPosition,
 }: {
   menu: MenuItem;
-  idx: number;
   activeMenuItem: MenuItem;
   handleButtonClick: (item: MenuItem) => void;
-  switchCustomFolderPosition: (id1: string, id2: string) => void;
 }) => {
   const isActive = menu.label === activeMenuItem.label;
   const activeStyle = isActive
@@ -41,70 +30,27 @@ export const MainMenuItem = ({
         } as any)
       : menu.icon;
 
-  const ref = React.useRef<HTMLDivElement>(null);
-
-  const [{ isOver, canDrop }, drop] = useDrop<DragItem, void, { isOver: boolean, canDrop:boolean }>({
-    accept: "MENUITEM",
-    canDrop: () => menu.menuType !== "default",
-    drop(item) {
-      const waitingItemId = menu.id;
-      const droppedItemId = item.menu.id;
-      if (item.index === idx) return;
-      switchCustomFolderPosition(waitingItemId, droppedItemId);
-      item.index = idx;
-    },
-    collect: (monitor) => ({
-      isOver: monitor.isOver({ shallow: true }),
-      canDrop: monitor.canDrop(),
-    }),
-  });
-
-  const [{ opacity,isDragging }, drag] = useDrag({
-    type: "MENUITEM",
-    item: { index: idx, type: "MENUITEM", menu },
-    canDrag: () => menu.menuType !== "default",
-    collect: (monitor) => ({
-      opacity: monitor.isDragging() ? 0.5 : 1,
-      isDragging: monitor.isDragging(),
-    }),
-  });
-
-  drag(drop(ref));
-
   return (
-    <div
-      ref={ref}
+    <Button
+      key={menu.label}
       style={{
-        opacity,
-        cursor: "move",
-        border: isOver && canDrop
-          ? `2px solid ${theme.palette.primary.main}`
-          : "2px solid transparent",
-        borderRadius: 8,
-        transition: "border-color 0.2s",
-      } as React.CSSProperties}
+        ...buttonStyle,
+        ...activeStyle,
+        borderTopRightRadius: "0px",
+        borderBottomRightRadius: "0px",
+      }}
+      onClick={() => handleButtonClick(menu)}
     >
-      <Button
-        key={menu.label}
-        style={{
-          ...buttonStyle,
-          ...activeStyle,
-          borderTopRightRadius: "0px",
-          borderBottomRightRadius: "0px",
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
         }}
-        onClick={() => handleButtonClick(menu)}
       >
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-          }}
-        >
-          {icon}
-          <span>{menu.label}</span>
-        </Box>
-      </Button>
-    </div>
+        {icon}
+        <span>{menu.label}</span>
+      </Box>
+    </Button>
   );
 };
