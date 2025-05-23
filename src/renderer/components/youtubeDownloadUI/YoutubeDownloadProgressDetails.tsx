7814205880360932f1350fb3e21f64173255884a
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2025-05-23 08:18:07 -0400

    add thumbnail to youtube download queue

--- src/renderer/components/youtubeDownloadUI/YoutubeDownloadProgressDetails.tsx
+++ src/renderer/components/youtubeDownloadUI/YoutubeDownloadProgressDetails.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Avatar, Box } from "@mui/material";
 import theme from "../../theme";
 import { YoutubeDownloadQueueItem } from "../../../main/services/youtube.service";
 
@@ -7,53 +7,47 @@ export const YoutubeDownloadProgressDetails = ({
 }: {
   item: YoutubeDownloadQueueItem;
 }) => (
-  <Box
-    className="flex flex-col gap-1"
-    sx={{
-      p: 1,
-      borderRadius: 1,
-      backgroundColor: theme.customVariables.appDark,
-      color: theme.customVariables.appWhiteSmoke,
-      minWidth: 0,
-      flex: 1,
-    }}
-  >
+  <Box className="flex gap-1">
+    <Avatar
+      variant="rounded"
+      src={item.poster}
+      alt={item.title}
+      sx={{ width: 80, height: 80 }}
+    />
     <Box
+      className="flex min-w-0 flex-1 flex-col gap-1 rounded p-1"
       sx={{
-        fontWeight: 600,
-        fontSize: "1rem",
+        backgroundColor: theme.customVariables.appDark,
         color: theme.customVariables.appWhiteSmoke,
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        textOverflow: "ellipsis",
       }}
-      title={item.title}
     >
-      {item.title}
-    </Box>
-    <Box
-      sx={{
-        fontSize: "0.85rem",
-        color: theme.palette.primary.light,
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        textOverflow: "ellipsis",
-      }}
-      title={item.url}
-    >
-      {item.url}
-    </Box>
-    <Box
-      sx={{
-        fontSize: "0.8rem",
-        color: theme.palette.grey[400],
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        textOverflow: "ellipsis",
-      }}
-      title={item.destinationPath}
-    >
-      {item.destinationPath}
+      <Box
+        className="overflow-hidden text-ellipsis whitespace-nowrap text-base font-semibold"
+        sx={{
+          color: theme.customVariables.appWhiteSmoke,
+        }}
+        title={item.title}
+      >
+        {item.title}
+      </Box>
+      <Box
+        className="overflow-hidden text-ellipsis whitespace-nowrap text-sm"
+        sx={{
+          color: theme.palette.primary.light,
+        }}
+        title={item.url}
+      >
+        {item.url}
+      </Box>
+      <Box
+        className="overflow-hidden text-ellipsis whitespace-nowrap text-xs"
+        sx={{
+          color: theme.palette.grey[400],
+        }}
+        title={item.destinationPath}
+      >
+        {item.destinationPath}
+      </Box>
     </Box>
   </Box>
 );
